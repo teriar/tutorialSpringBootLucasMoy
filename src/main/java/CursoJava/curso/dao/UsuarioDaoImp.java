f1c2e24d8168fca37189c51b2bfcc9da872de937
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-08-06 21:49:11 -0400

    inicio jwt

--- src/main/java/CursoJava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/CursoJava/curso/dao/UsuarioDaoImp.java
@@ -1,6 +1,8 @@
 package CursoJava.curso.dao;
 
 import CursoJava.curso.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,13 +41,20 @@ public class UsuarioDaoImp implements  UsuarioDao{
     @Override
     public boolean verificarCredenciales(Usuario usuario){
         System.out.print("dsa");
-        String query=" FROM Usuario WHERE email = :email AND password = :password ";
-         List<Usuario> lista = entityManeger.createQuery(query)
-                 .setParameter(  "email", usuario.getEmail())
-                 .setParameter("password", usuario.getPassword())
-                 .getResultList();
+        String query=" FROM Usuario WHERE email = :email ";
+        List<Usuario> lista = entityManeger.createQuery(query)
+                .setParameter(  "email", usuario.getEmail())
+                .getResultList();
+
+        if(lista.isEmpty()){
+            return false;
+        }
+
+        String passworHashed = lista.get(0).getPassword();
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        return  argon2.verify(passworHashed,usuario.getPassword());
 
 
-         return !lista.isEmpty();
     }
 }
